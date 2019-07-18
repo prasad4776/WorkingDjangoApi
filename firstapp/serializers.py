9f67d1428b692e25d6bb8a2afe926d0570451d30
Prasad Lagas <prasad.lagas83@gmail.com>
2019-07-18 15:41:25 -0400

    Done

---
+++ firstapp/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from . models import Customer
+
+
+class CustomerSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Customer
+        fields = ('id','name','city','url')
+        
